a4d3ac04d29bee1a3cbc01fb948bf5662b8c9319
Linda <fatlinda.islami@mni.thm.de>
2025-05-05 18:43:26 +0200

    bot fix

--- backend/src/bot/bot.module.ts
+++ backend/src/bot/bot.module.ts
@@ -1,8 +1,12 @@
 // src/bot/bot.module.ts
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Questionnaire } from '../Questionnaire/questionnaire.entity';
+import { Answer } from '../Answer/answer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Questionnaire, Answer])],
   providers: [BotService],
   exports: [BotService],
 })
